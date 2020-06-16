ffa5223a944a64b42efbc2e47410450d15b93932
alvaro <a@yomail.com>
2020-06-16 02:44:41 -0500

    Se agrega Slider de imagenes en el home

--- app/src/main/java/com/example/proydami/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/proydami/ui/home/HomeFragment.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.proydami.R
+import com.example.proydami.R.id.carousel
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.*
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    var carouselImages = intArrayOf(
+        R.drawable.img1,
+        R.drawable.img2,
+        R.drawable.img3
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +34,22 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+
+
         homeViewModel.text.observe(this, Observer {
-            textView.text = it
+            val carouselView = view?.findViewById(carousel) as? CarouselView;
+            carouselView?.setPageCount(carouselImages.size);
+            carouselView?.setImageListener(imageListener);
         })
         return root
     }
+
+    var imageListener: ImageListener = object : ImageListener {
+        override fun setImageForPosition(position: Int, imageView: ImageView) {
+            // You can use Glide or Picasso here
+            imageView.setImageResource(carouselImages[position])
+        }
+    }
+
+
 }
