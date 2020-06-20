0eb2f8bdb7655e4cc5502a417514a5211e88eafc
alvaro <a@yomail.com>
2020-06-20 02:36:48 -0500

    Fix 1 fragment on map

--- app/src/main/java/com/example/proydami/ui/contactus/ContactusFragment.kt
+++ app/src/main/java/com/example/proydami/ui/contactus/ContactusFragment.kt
@@ -1,10 +1,12 @@
 package com.example.proydami.ui.contactus
 
 import android.os.Bundle
+import android.view.InflateException
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -14,60 +16,82 @@ import com.google.android.gms.maps.*
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_contactus.view.*
 
 
-class ContactusFragment : Fragment(), OnMapReadyCallback {
+class ContactusFragment : Fragment() {
 
     private lateinit var contactusViewModel: ContactusViewModel
 
     private lateinit var mMap: GoogleMap
-    var mapView: MapView? = null
+    //var mapView: MapView? = null
+    var root: View? = null
 
-    companion object {
-        var mapFragment : SupportMapFragment?=null
+    private var mapReady = false
 
-        val TAG: String = MapFragment::class.java.simpleName
-        fun newInstance() = MapFragment()
-    }
+//    companion object {
+//        var mapFragment : SupportMapFragment?=null
+//
+//        val TAG: String = MapFragment::class.java.simpleName
+//        fun newInstance() = MapFragment()
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        contactusViewModel =
-            ViewModelProviders.of(this).get(ContactusViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_contactus, container, false)
 
-        mapFragment = fragmentManager?.findFragmentById(R.id.contactMap) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
 
-        //mapView = root.findViewById(R.id.contactMap)
-       // mapView.onCreate(savedInstanceState)
-//        if (mapView != null) {
-//            mMap = mapView.getMap
-//            mMap.getUiSettings().setMyLocationButtonEnabled(false)
-//            mMap.setMyLocationEnabled(true)
-//            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(43.1, -87.9), 10f)
-//            mMap.animateCamera(cameraUpdate)
-//        }
-        contactusViewModel.text.observe(this, Observer {
+            if (root != null) {
+                var parent:ViewGroup = root?.getParent() as ViewGroup;
+                if (parent != null)
+                    parent.removeView(root);
+            }
+            try {
+                root = inflater.inflate(R.layout.fragment_contactus, container, false)
+            } catch (e: InflateException) {
+                val toast = Toast.makeText(activity, e.message, Toast.LENGTH_LONG)
+                toast.show()
+            }
+
+        //contactusViewModel = ViewModelProviders.of(this).get(ContactusViewModel::class.java)
+
+        var mapFragment = fragmentManager?.findFragmentById(R.id.contactMap) as SupportMapFragment?
+        //mapFragment?.getMapAsync(this)
+
+        mapFragment?.getMapAsync {
+                googleMap -> mMap = googleMap
+            mapReady = true
+            setValuesonMapReady()
+        }
+
+        //contactusViewModel.text.observe(this, Observer {
             //textView.text = it
 //            val mapFragment = supportFragmentManager
 //                .findFragmentById(R.id.contactMap) as SupportMapFragment
 //            mapFragment.getMapAsync(this)
-        })
+        //})
         return root
     }
 
-    override fun onMapReady(gm: GoogleMap) {
-        mMap = gm
+    fun setValuesonMapReady() {
+        //mMap = gm
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+//    override fun onMapReady(gm: GoogleMap) {
+//        mMap = gm
+//
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//    }
 
 
 }
