99322bdb72df231e10b1c4926432dfd745a64329
alvaro <a@yomail.com>
2020-07-08 21:12:31 -0500

    #Se agrega función agregar a carrito

--- app/src/main/java/com/example/proydami/ProductosAdapters/PrdDetailActivity.kt
+++ app/src/main/java/com/example/proydami/ProductosAdapters/PrdDetailActivity.kt
@@ -1,18 +1,25 @@
 package com.example.proydami.ProductosAdapters
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.proydami.Models.Producto
 import com.example.proydami.R
+import com.example.proydami.SqlLight.CarritoDBHelper
+import com.example.proydami.SqlLight.Columns
 import kotlinx.android.synthetic.main.activity_prd_detail.*
 
 class PrdDetailActivity : AppCompatActivity() {
+
+    var cartDB : CarritoDBHelper? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prd_detail)
 
-        var extras = intent.extras
+        val extras = intent.extras
 
         if (extras != null){
             val p = extras["PRDs"] as? Producto
@@ -22,4 +29,28 @@ class PrdDetailActivity : AppCompatActivity() {
 
         }
     }
+
+
+    fun agregarAlCarrito(nombre:String, poder: String, codigo: String){
+        val bd = cartDB!!.writableDatabase
+
+        val values = ContentValues().apply {
+            put(Columns.CartBC._CODIGO, codigo.toInt() )
+            put(Columns.CartBC._NOMBRE, nombre)
+            put(Columns.CartBC._PRECIO, poder)
+            put(Columns.CartBC._DESC, poder)
+        }
+
+        val newRowId = bd?.insert(Columns.CartBC._TABLA, null, values)
+
+        if(newRowId != (-1).toLong()){
+            Toast.makeText(applicationContext,"Se insertó", Toast.LENGTH_SHORT).show()
+
+            println("inserto")
+        }else{
+            Toast.makeText(applicationContext,"Valor ya registrado", Toast.LENGTH_SHORT).show()
+            println("no inserto")
+        }
+    }
+
 }
